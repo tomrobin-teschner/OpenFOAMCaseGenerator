2181db8b3c17033ce701f3727b5a141efdea7f30
Tom-Robin Teschner <tomrobin.teschner@yahoo.de>
2020-11-14 08:58:30 +0000

    Added variable dictionaries
    Both the files and flow properties are now
    grouped into dictionaries. Changes have
    been propagated down to the
    corresponding function calls. File writing
    still needs to be updated for the 0 and
    system folder, i.e. convert function calls
    to classes.

--- FileDirectoryIO/FileManager.py
+++ FileDirectoryIO/FileManager.py
@@ -2,16 +2,14 @@ import os
 
 
 class FileManager:
-    def __init__(self, case_name, path, version):
-        self.case_name = case_name
-        self.path = path
-        self.version = version
+    def __init__(self, file_properties):
+        self.file_properties = file_properties
 
     def create_directory_structure(self):
-        self.__create_directory(os.path.join(self.path, self.case_name))
-        self.__create_directory(os.path.join(self.path, self.case_name, '0'))
-        self.__create_directory(os.path.join(self.path, self.case_name, 'constant'))
-        self.__create_directory(os.path.join(self.path, self.case_name, 'system'))
+        self.__create_directory(os.path.join(self.file_properties['path']))
+        self.__create_directory(os.path.join(self.file_properties['path'], '0'))
+        self.__create_directory(os.path.join(self.file_properties['path'], 'constant'))
+        self.__create_directory(os.path.join(self.file_properties['path'], 'system'))
         self.__create_case_file()
 
     def __create_directory(self, directory):
@@ -19,24 +17,24 @@ class FileManager:
             os.makedirs(directory)
 
     def __create_case_file(self):
-        file_id = open(os.path.join(self.path, self.case_name, self.case_name + '.foam'), 'w')
+        file_id = open(os.path.join(self.file_properties['path'], self.file_properties['case_name'] + '.foam'), 'w')
         file_id.close()
 
     def create_file(self, folder, file_name):
-        file_id = open(os.path.join(self.path, self.case_name, folder, file_name), 'w')
+        file_id = open(os.path.join(self.file_properties['path'], folder, file_name), 'w')
         return file_id
 
     def close_file(self, file_id):
-        file_id.close();
+        file_id.close()
 
     def write(self, file_id, message):
         file_id.write(message)
 
-    def write_header(self, file_id, class_type, location, object):
+    def write_header(self, file_id, class_type, location, object_type):
         file_id.write('/*--------------------------------*- C++ -*----------------------------------*\\\n')
         file_id.write('| =========                 |                                                 |\n')
         file_id.write('| \\\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox           |\n')
-        file_id.write('|  \\\    /   O peration     | Version:  ' + self.version + '                                 |\n')
+        file_id.write('|  \\\    /   O peration     | Version:  ' + self.file_properties['version'] + '                                 |\n')
         file_id.write('|   \\\  /    A nd           | Web:      www.OpenFOAM.com                      |\n')
         file_id.write('|    \\\/     M anipulation  |                                                 |\n')
         file_id.write('\*---------------------------------------------------------------------------*/\n')
@@ -46,6 +44,6 @@ class FileManager:
         file_id.write('    format      ascii;\n')
         file_id.write('    class       ' + class_type + ';\n')
         file_id.write('    location    "' + location + '";\n')
-        file_id.write('    object      ' + object + ';\n')
+        file_id.write('    object      ' + object_type + ';\n')
         file_id.write('}\n')
         file_id.write('// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //\n')
--- WriteVelocity.py
+++ WriteVelocity.py
@@ -1,39 +1,39 @@
-import os
 import GlobalVariables as Parameters
-import BoundaryConditions as boundary_conditions
+import BoundaryConditions as BoundaryConditions
 
 
-def write_boundary_condition(file_manager, BC, outlet_type, velocity):
+def write_boundary_condition(file_manager, boundary_properties, outlet_type, flow_properties):
 
     # create new boundary file
     file_id = file_manager.create_file('0', 'U')
     file_manager.write_header(file_id, 'volVectorField', '0', 'U')
 
-    # write dimensions and internfield
-    initial_field = 'uniform (' + str(velocity[0]) + ' ' + str(velocity[1]) + ' ' + str(velocity[2]) + ')'
+    # write dimensions and internal-field
+    initial_field = ('uniform (' + str(flow_properties['inlet_velocity'][0]) + ' ' +
+                     str(flow_properties['inlet_velocity'][1]) + ' ' + str(flow_properties['inlet_velocity'][2]) + ')')
     file_manager.write(file_id, '\ndimensions      [0 1 -1 0 0 0 0];\n\ninternalField   ' + initial_field + ';\n\n')
 
     # write boundary conditions
     file_manager.write(file_id, 'boundaryField\n{\n')
-    for key in BC:
+    for key in boundary_properties:
         file_manager.write(file_id, '    ' + key + '\n    {\n')
-        if BC[key] == Parameters.WALL:
-            boundary_conditions.no_slip_wall(file_id)
-        elif BC[key] == Parameters.OUTLET:
+        if boundary_properties[key] == Parameters.WALL:
+            BoundaryConditions.no_slip_wall(file_id)
+        elif boundary_properties[key] == Parameters.OUTLET:
             if outlet_type == Parameters.NEUMANN:
-                boundary_conditions.neumann(file_id)
+                BoundaryConditions.neumann(file_id)
             elif outlet_type == Parameters.ADVECTIVE:
-                boundary_conditions.advective(file_id)
+                BoundaryConditions.advective(file_id)
             elif outlet_type == Parameters.INLET_OUTLET:
-                boundary_conditions.inlet_outlet(file_id, initial_field)
-        elif BC[key] == Parameters.SYMMETRY:
-            boundary_conditions.neumann(file_id)
-        elif BC[key] == Parameters.INLET:
-            boundary_conditions.dirichlet(file_id, initial_field)
-        elif BC[key] == Parameters.CYCLIC:
-            boundary_conditions.periodic(file_id)
-        elif BC[key] == Parameters.EMPTY:
-            boundary_conditions.empty(file_id)
+                BoundaryConditions.inlet_outlet(file_id, initial_field)
+        elif boundary_properties[key] == Parameters.SYMMETRY:
+            BoundaryConditions.neumann(file_id)
+        elif boundary_properties[key] == Parameters.INLET:
+            BoundaryConditions.dirichlet(file_id, initial_field)
+        elif boundary_properties[key] == Parameters.CYCLIC:
+            BoundaryConditions.periodic(file_id)
+        elif boundary_properties[key] == Parameters.EMPTY:
+            BoundaryConditions.empty(file_id)
         file_manager.write(file_id, '    }\n')
 
     file_manager.write(file_id, '}')
